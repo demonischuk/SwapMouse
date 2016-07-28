075b6726c9a55a8c0ecadd6d64330e0418a250ee
demonischuk <demonisch@hotmail.co.uk>
2016-07-28 20:32:08 +0100

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace SwapMouse
+{
+    partial class frmSwapMouse
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rdoRight = new System.Windows.Forms.RadioButton();
+            this.rdoLeft = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rdoRight
+            // 
+            this.rdoRight.AutoSize = true;
+            this.rdoRight.Checked = true;
+            this.rdoRight.Location = new System.Drawing.Point(25, 58);
+            this.rdoRight.Name = "rdoRight";
+            this.rdoRight.Size = new System.Drawing.Size(93, 29);
+            this.rdoRight.TabIndex = 0;
+            this.rdoRight.TabStop = true;
+            this.rdoRight.Text = "Right";
+            this.rdoRight.UseVisualStyleBackColor = true;
+            this.rdoRight.CheckedChanged += new System.EventHandler(this.FlipMouse);
+            // 
+            // rdoLeft
+            // 
+            this.rdoLeft.AutoSize = true;
+            this.rdoLeft.Location = new System.Drawing.Point(164, 58);
+            this.rdoLeft.Name = "rdoLeft";
+            this.rdoLeft.Size = new System.Drawing.Size(79, 29);
+            this.rdoLeft.TabIndex = 1;
+            this.rdoLeft.Text = "Left";
+            this.rdoLeft.UseVisualStyleBackColor = true;
+            this.rdoLeft.CheckedChanged += new System.EventHandler(this.FlipMouse);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdoRight);
+            this.groupBox1.Controls.Add(this.rdoLeft);
+            this.groupBox1.Location = new System.Drawing.Point(12, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(298, 132);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Set mouse to";
+            // 
+            // frmSwapMouse
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 169);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmSwapMouse";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mouse Swap";
+            this.TopMost = true;
+            this.Activated += new System.EventHandler(this.FocussedForm);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rdoRight;
+        private System.Windows.Forms.RadioButton rdoLeft;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace SwapMouse
+{
+    public partial class frmSwapMouse : Form
+    {
+        private const int RIGHT_HANDED = 0;
+        private const int LEFT_HANDED = 1;
+
+        [DllImport("user32.dll")]
+        public static extern Int32 SwapMouseButton(Int32 bSwap);
+
+        public frmSwapMouse()
+        {
+            InitializeComponent();
+            this.DisplayCurrentMouseSetup();
+        }
+
+        private void FlipMouse(object sender, EventArgs e)
+        {
+            SwapMouseButton(this.rdoRight.Checked ? RIGHT_HANDED : LEFT_HANDED);
+        }
+
+        private void FocussedForm(object sender, EventArgs e)
+        {
+            this.DisplayCurrentMouseSetup();
+        }
+
+        private void DisplayCurrentMouseSetup()
+        {
+            //There is not a direct method for obtaining the current mouse direction
+            //instead calling the swap method will return 0 if the direction was previously right handed, else non zero if previously left handed
+            var mouseIsCurrentlyLeftHanded = SwapMouseButton(LEFT_HANDED) != 0;
+
+            //restore whatever direction the mouse was set to
+            SwapMouseButton(mouseIsCurrentlyLeftHanded ? LEFT_HANDED : RIGHT_HANDED);
+
+            //display to the user what direction the mouse is currently set as
+            this.rdoLeft.Checked = mouseIsCurrentlyLeftHanded;
+        }
+    }
+}
